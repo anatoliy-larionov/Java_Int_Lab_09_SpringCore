b5ee28c10d9a07f0f7be408bfda28ba96dad01b9
anatolii_larionov <adlmidl@yandex.ru>
2019-01-02 15:58:12 +0400

    init-1

---
+++ shop/src/main/java/org/shop/configuration/RepositoryConfig.java
@@ -0,0 +1,57 @@
+package org.shop.configuration;
+
+import org.shop.repository.*;
+import org.shop.repository.factory.UserRepositoryFactory;
+import org.shop.repository.map.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+@Configuration
+@PropertySource("classpath:resource/application.properties")
+public class RepositoryConfig {
+
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public ItemRepository itemRepository() {
+        return new ItemMapRepository();
+    }
+
+    @Bean
+    public OrderRepository orderRepository() {
+        OrderMapRepository orderMapRepository = new OrderMapRepository();
+        String intitialSequence = env.getProperty("intitialSequence");
+        assert intitialSequence != null;
+        orderMapRepository.setSequence(Long.parseLong(intitialSequence));
+        return orderMapRepository;
+    }
+
+    @Bean
+    public ProductRepository productRepository() {
+        return new ProductMapRepository();
+    }
+
+    @Bean
+    public ProposalRepository proposalRepository() {
+        return new ProposalMapRepository();
+    }
+
+    @Bean
+    public SellerRepository sellerRepository() {
+        return new SellerMapRepository();
+    }
+
+    @Bean
+    public UserRepositoryFactory userRepositoryFactory() {
+        return new UserRepositoryFactory();
+    }
+
+    @Bean
+    public UserRepository userRepository() {
+        return userRepositoryFactory().createUserRepository();
+    }
+}
